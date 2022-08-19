59aab1d45c5617dbe787c1ddc115653d9fd78e81
Artur <art05xxx@rambler.ru>
2022-08-19 17:18:45 +0300

    added new routers

--- app/alan/apps/docs/urls.py
+++ app/alan/apps/docs/urls.py
@@ -7,8 +7,10 @@ app_name = 'apps.docs'
 
 router = DefaultRouter()
 router.register(r'speciality', views.SpecialityViewSet, basename='speciality')
+router.register(r'category', views.CategoryViewSet, basename='category')
+router.register(r'information', views.InformationViewSet, basename='information')
+router.register(r'document', views.DocumentViewSet, basename='document')
 
 urlpatterns = [
     path('', include(router.urls)),
 ]
-
