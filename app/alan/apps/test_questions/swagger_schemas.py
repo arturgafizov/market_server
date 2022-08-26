1859dd2056db5c8dcc8d6ea1ac4415e2241ba17a
Artur <art05xxx@rambler.ru>
2022-08-26 15:28:32 +0300

    added new description

---
+++ app/alan/apps/test_questions/swagger_schemas.py
@@ -0,0 +1,20 @@
+tags_result = ['Test result']
+tags_question_answer_user = ['Test question answer user']
+
+tags_result_create = {
+    'operation_description': '## Создание результатов тестов',
+    'operation_summary': 'Создание результатов тестов',
+    'tags': tags_result,
+}
+
+tags_question_answer_user_create = {
+    'operation_description': '## Создание ответа пользователя на вопрос',
+    'operation_summary': 'Создание ответа пользователя на вопрос',
+    'tags': tags_question_answer_user,
+}
+
+tags_question_answer_user_list = {
+    'operation_description': '## Получение списка результатов тестирования пользователя',
+    'operation_summary': 'Получение списка результатов пользователя',
+    'tags': tags_question_answer_user,
+}
